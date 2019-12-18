2c17259f5cf5b7a1bced975fd6e08e0ca41c4980
elliottwarren <e.l.warren@pgr.reading.ac.uk>
2019-12-18 15:11:05 +0000

    functional but slow... commit before editing SQL code to save locations as variables, for later reuse.

--- scripts/obs_analyse.py
+++ scripts/obs_analyse.py
@@ -3,6 +3,8 @@
 """
 Script to analyse the observations that get integrated into the data assimilation process, for a suite run
 
+Ensure PATH=$PATH:~odb/installs/odbapi/odbapi-0.10.3-ukmo-v2/gnu-4.4.7/bin is ran in the shell first!
+
 Created by Elliott Warren - Wed 11th Dec 2019: elliott.warren@metoffice.gov.uk
 """
 
@@ -74,6 +76,7 @@ if __name__ == '__main__':
     datadir = projectdir + '/data'
     plotdir = projectdir + '/figures'
     scratchdir = SCRATCH + '/ODB2'
+    logdir = scratchdir + '/log'
     numpysavedir = scratchdir + '/cycle_sql_stats'
 
     # suite dictionary
@@ -103,14 +106,21 @@ if __name__ == '__main__':
     cycle_range_str = [i.strftime('%Y%m%dT%H%M') for i in cycle_range]
 
     # ensure SCRATCH subdirectories are present for the ODB stats to be copied into, before further processing
-    for d in [scratchdir, numpysavedir]:
-        os.system('mkdir -p '+d)
+    for d in [scratchdir, logdir, numpysavedir]:
+        if not os.path.exists(d):
+            os.system('mkdir -p '+d)
 
     # flag headers to check for and create statistics about
     flags = ['active', 'rejected']
 
     # regions to create statistic entries for
     regions = ['SH', 'NH', 'TR', 'global']
+
+    # create empty log file that will be filled with filepaths of bad files, if any are present
+    run_time_str = dt.datetime.now().strftime('%Y%m%dT%H%M')
+    log_file_path = logdir + '/' + run_time_str + '_obs_anaylse_log.txt'
+    with open(log_file_path, 'w') as log_file:
+        log_file.write('log for obs_analyse - ran at '+run_time_str+'/n')
 
     # ==============================================================================
     # Process
@@ -174,33 +184,52 @@ if __name__ == '__main__':
                 # name of file without the .gz extension
                 filepath_unzipped = filepath[:-3]
 
-                # where_constriant = datum_status.active or datum_status.rejected
+                # try to do SQL queries but if the file is corrupt, the querries will fail.
+                # If file is corrupt - catch exception, remove bad file and add it to a log of bad filenames
+                try:
 
-                # header flags to check for in ODB databases
-                for flag_i in flags:
+                    # header flags to check for in ODB databases
+                    for flag_i in flags:
 
-                    print '... ... ... working on flag: '+flag_i
+                        print '... ... ... working on flag: '+flag_i
 
-                    # count number of observations that were included in the data assimilation,
-                    #   for this ob type, cycle, suite.
+                        # count number of observations that were included in the data assimilation,
+                        #   for this ob type, cycle, suite.
 
-                    # Southern hemisphere
-                    statement = 'odb sql \'select count(*) where (datum_status.'+flag_i+') and (lat <= \'0\') \' -i '+filepath_unzipped
-                    suite_cycle_stats[flag_i]['SH'][obs_i] = sql_count_all_query(statement)
+                        # Southern hemisphere
+                        statement = 'odb sql \'select count(*) where (datum_status.'+flag_i+') and (lat <= \'0\') \' -i '+filepath_unzipped
+                        suite_cycle_stats[flag_i]['SH'][obs_i] = sql_count_all_query(statement)
 
-                    # Northern hemisphere
-                    statement = 'odb sql \'select count(*) where (datum_status.'+flag_i+') and (lat >= \'0\') \' -i '+filepath_unzipped
-                    suite_cycle_stats[flag_i]['NH'][obs_i] = sql_count_all_query(statement)
+                        # # test alternative for SH
+                        # statement = 'odb sql \'select count(case (datum_status.'+flag_i+') and (lat <= \'0\')) \' -i '+filepath_unzipped
+                        # suite_cycle_stats[flag_i]['SH'][obs_i] = sql_count_all_query(statement)
 
-                    # Tropics are 20 N to 20 S
-                    statement = 'odb sql \'select count(*) where (datum_status.'+flag_i+') and (lat < \'20\' and lat > \'-20\') \' -i '+filepath_unzipped
-                    suite_cycle_stats[flag_i]['TR'][obs_i] = sql_count_all_query(statement)
+                        # save NH location for later?
+                        statement = 'odb sql \'select @NH := where lat >= \'0\'\' -i ' + filepath_unzipped
+                        out = subprocess.check_output(statement, shell=True)
+                        split = out.strip(' ').split('\n')
 
-                    # global
-                    suite_cycle_stats[flag_i]['global'][obs_i] = \
-                        suite_cycle_stats[flag_i]['NH'][obs_i] + suite_cycle_stats[flag_i]['SH'][obs_i]
+                        # Northern hemisphere
+                        statement = 'odb sql \'select count(*) where (datum_status.'+flag_i+') and (lat >= \'0\') \' -i '+filepath_unzipped
+                        suite_cycle_stats[flag_i]['NH'][obs_i] = sql_count_all_query(statement)
 
-                # remove file once its done with
+                        # Tropics are 20 N to 20 S
+                        statement = 'odb sql \'select count(*) where (datum_status.'+flag_i+') and (lat < \'20\' and lat > \'-20\') \' -i '+filepath_unzipped
+                        suite_cycle_stats[flag_i]['TR'][obs_i] = sql_count_all_query(statement)
+
+                        # global
+                        suite_cycle_stats[flag_i]['global'][obs_i] = \
+                            suite_cycle_stats[flag_i]['NH'][obs_i] + suite_cycle_stats[flag_i]['SH'][obs_i]
+
+                        # delete from SQL the active flags?
+
+                except:
+                    print '/n/n!!!'+filepath_unzipped +' is a bad file!!!/n/n'
+                    # add bad file to the log
+                    with open(log_file_path, 'a') as log_file:
+                        log_file.write(filepath_unzipped+'/n')
+
+                # remove file once its done with (whether a good or bad file)
                 os.remove(filepath_unzipped)
 
             print '... ... computing observation totals, across flags'
@@ -219,6 +248,7 @@ if __name__ == '__main__':
             np.save(numpysavepath, suite_cycle_stats)
 
             print '... ... '+numpysavepath+' saved!'
+            print '/n/n/n'
 
     exit(0)
 
